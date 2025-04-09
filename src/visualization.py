57a5296bb30e13ac94d8bdd1a2351064db59da33
FlorianJacta <98709993+FlorianJacta@users.noreply.github.com>
2025-04-09 09:42:29 +0200

    Add all visualizations

--- src/visualization.py
+++ src/visualization.py
@@ -6,18 +6,35 @@ script_dir = Path(__file__).resolve().parent
 
 # Build paths to data files relative to the script location
 data_dir = script_dir / "data"
-surface_path = data_dir / "surface_car.vtp"
-volume_path = data_dir / "volume_spoiler.vtu"
+surface_car_path = data_dir / "surface_car.vtp"
+volume_car_path = data_dir / "volume_car.vtp"
+surface_spoiler_path = data_dir / "surface_spoiler.vtp"
+volume_spoiler_path = data_dir / "volume_spoiler.vtu"
+
 
 # Load and plot the car surface
-surface_car = pv.read(surface_path)
+surface_car = pv.read(surface_car_path)
 surface_car.plot(
     scalars=None,
     show_edges=True,
 )
 
+# Load and plot the car volume
+volume_car = pv.read(volume_car_path)
+volume_car.plot(
+    scalars="pressure",
+    cmap="coolwarm",
+)
+
+# Load and plot the spoiler surface
+surface_spoiler = pv.read(surface_spoiler_path)
+surface_spoiler.plot(
+    scalars=None,
+    show_edges=True,
+)
+
 # Load and plot the spoiler volume
-volume_spoiler = pv.read(volume_path)
+volume_spoiler = pv.read(volume_spoiler_path)
 volume_spoiler.plot(
     scalars="pressure",
     cmap="coolwarm",
